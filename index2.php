602226f0df7857166c4c661cef54999219020a1b
ralmarri12 <r.almarri12@gmail.com>
2013-10-05 13:05:50 +0800

    working with bs

---
+++ index2.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+
+	
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+		
+		
+		<!--Google fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Economica|Cookie' rel='stylesheet' type='text/css'>
+		
+		<!--Engazy style sheet -->
+		<link rel="stylesheet" href="css/style.css">
+		
+		<!--Fontello icons fonts -->
+		<link rel="stylesheet" href="css/fontello.css">
+		<link rel="stylesheet" href="css/animation.css"><!--[if IE 7]>
+		<link rel="stylesheet" href="css/fontello-ie7.css"><![endif]-->
+		<script>
+			function toggleCodes(on) {
+				var obj = document.getElementById('icons');
+
+				if (on) {
+					obj.className += ' codesOn';
+				} else {
+					obj.className = obj.className.replace(' codesOn', '');
+				}
+			}
+		</script>
+		
+	
+		<title> :: Page title :: </title>
+		
+		
+		
+	</head>
+	
+	<body>
+		<div id="header">
+			<div id="warp">
+					<h1>Enjazy</h1>
+			</div>
+		</div>
+		
+		<div id="navbar">
+			<div id="warp">
+			<ul>
+				<li><i class="icon-home"></i></li>
+				<li>Services</li>
+				<li>Our Work</li>
+				<li>About Us</li>
+				<li>Contact Us</li>
+			</ul>
+			</div>
+		</div>
+
+		
+		<div id="warp">
+			<div class="icons">
+				<ul>
+					<li><i class="icon-facebook-squared"></i></li>
+					<li><i class="icon-twitter"></i></li>
+					<li><i class="icon-gplus"></i></li>
+					<li><i class="icon-rss-alt"></i></li>
+				</ul>
+			</div>
+		
+		</div>
+	
+	</body>
+	
+	
+</html>
