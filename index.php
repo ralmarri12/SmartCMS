6f3a447c0f254c7044036d2fe6b0499f0c4e6b2f
ralmarri12 <r.almarri12@gmail.com>
2013-10-05 14:45:28 +0800

    Editing index.php and uplouding one content image

--- index.php
+++ index.php
@@ -50,30 +50,73 @@
 		
 		
 		
-		<div id="navbar">
-			<div id="warp">
-			<ul>
-				<li><i class="icon-home"></i></li>
-				<li>Services</li>
-				<li>Our Work</li>
-				<li>About Us</li>
-				<li>Contact Us</li>
-			</ul>
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Project name</a>
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#"><i class="icon-home"></i></a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Our Work</a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+					
+				</div>
 			</div>
 		</div>
 
+		<!-- ======================================= -->
 		
-		<div id="warp">
-			<div class="icons">
-				<ul>
-					<li><i class="icon-facebook-squared"></i></li>
-					<li><i class="icon-twitter"></i></li>
-					<li><i class="icon-gplus"></i></li>
-					<li><i class="icon-rss-alt"></i></li>
-				</ul>
+		<div class="jumbotron">
+			<div class="container">
+				<div class="icons">
+					<ul>
+						<li><i class="icon-facebook-squared"></i></li>
+						<li><i class="icon-twitter"></i></li>
+						<li><i class="icon-gplus"></i></li>
+						<li><i class="icon-rss-alt"></i></li>
+					</ul>
+				</div>
+				
+			
+				<h1> Hello, world! </h1>
+				<p>This is a template for a simple marketing or informational website.
+				It includes a large callout called the hero unit and three supporting pieces of content.
+				Use it as a starting point to create something more unique.</p>
+				<p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
 			</div>
 		</div>
-	
+		
+		<!-- ======================================= -->
+		<hr class="featurette-divider">
+		
+		<div class="container">
+		
+		<div class="row featurette">
+			<div class="col-md-7">
+				<h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+				<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+			</div>
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive" src="images/Eiffel-Tower.jpg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
+		
+		</div>
+		
+		
+		
+		
+		<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</body>
 	
 	
